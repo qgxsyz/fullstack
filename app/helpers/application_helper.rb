0be51185c5a5fc161f158e19a00f4a6aadced7fa
shistriver <954917356@qq.com>
2017-10-15 00:14:10 +0800

    课程预计学习时间 课程总作业数

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,26 @@ module ApplicationHelper
   def is_all_course?
     "active" if params[:category] ==  nil
   end
+
+  #每个 课程预计学习时间
+  def totalLearningTime(chapters)
+    total_time = 0 #总时间
+    chapters.each do |chapter|
+      total_time += chapter.learning_time
+    end
+    total_time
+  end
+
+  #每个 课程总作业数
+  def totalAssignments(chapters)
+    total_assignment = 0 #课程作业数
+    chapter_assignment = 0 #章作业数
+    chapters.each do |chapter|
+      chapter.sections.each do |section|
+        chapter_assignment += section.assignments.length
+      end
+    end
+    total_assignment += chapter_assignment
+  end
+
 end
