deb9c17f06481b0c34cdb3b96693d9548fb69d28
qiuguixin <qguixin@qq.com>
2017-10-15 21:34:39 +0800

    对seeds中mock用户进行了判断

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,17 +8,27 @@
 # rake db:reset 日后资料库设定 ( migrate ) 重建时发生错误时的 bug fix
 
 # mock一个管理员账户 执行rake db:seed
-u = User.new
-u.email = "admin@test.com"           # 可以改成自己的 email
-u.password = "123456"                # 最少要六码
-u.password_confirmation = "123456"   # 最少要六码
-u.is_admin = true
-u.save
+if User.find_by(email: "admin@test.com").nil?
+  u = User.new
+  u.email = "admin@test.com"           # 可以改成自己的 email
+  u.password = "123456"                # 最少要六码
+  u.password_confirmation = "123456"   # 最少要六码
+  u.is_admin = true
+  u.save
+  puts "Admin 已经建立好了，帐号为#{u.email}, 密码为#{u.password}"
+else
+  puts "Admin 已经建立过了，脚本跳过该步骤。"
+end
 
 # mock一个普通账户 执行rake db:seed
-u = User.new
-u.email = "qiuguixin@test.com"       # 可以改成自己的 email
-u.password = "123456"                # 最少要六码
-u.password_confirmation = "123456"   # 最少要六码
-u.is_admin = false
-u.save
+if User.find_by(email: "admin@test.com").nil?
+  u = User.new
+  u.email = "student@test.com"       # 可以改成自己的 email
+  u.password = "123456"                # 最少要六码
+  u.password_confirmation = "123456"   # 最少要六码
+  u.is_admin = false
+  u.save
+  puts "Student 已经建立好了，帐号为#{u.email}, 密码为#{u.password}"
+else
+  puts "Student 已经建立过了，脚本跳过该步骤。"
+end
