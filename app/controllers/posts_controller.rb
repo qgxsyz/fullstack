2db2ca0734ab33704a287c94bd6749132dc1329f
shistriver <954917356@qq.com>
2017-10-23 23:44:43 +0800

    显示作业

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,14 @@ class PostsController < ApplicationController
     begin
       @section = Section.find(params[:id])
       @course = @section.chapter.course
+      @section2 = @course.chapters
+      
+      @currentId = params[:id]
+      @firstId = @course.chapters.first.sections.first.id
+      @lastId = @course.chapters.last.sections.last.id
+      @next = Section.offset(1)
+      puts params[:id]
+      puts @section.chapter.sections.count
     rescue Exception => e
       render :template => "error/404/index.html.erb" #异常跳转
     else
