3db40886e7ba31858b5373f79001d492acc60f43
shistriver <954917356@qq.com>
2017-12-06 00:15:51 +0800

    给章节添加分页功能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,37 @@
 class PostsController < ApplicationController
 
   def show
-    begin
-      @section = Section.find(params[:id])
-      @course = @section.chapter.course
-      # @page = @section.joins(:chapter, :course)
-    rescue Exception => e
-      render :template => "error/404/index.html.erb" #异常跳转
-    else
+    @section = Section.find(params[:id])
+    @course = @section.chapter.course
+    @chapters = @course.chapters
+
+    # 分页 功能
+    pageArr = []
+    @chapters.each do |chapter|
+      chapter.sections.each do |section|
+        pageArr.push(section.id)
+      end
     end
+
+    @prevPage = ''
+    @nextPage = ''
+    currentPage = params[:id].to_i
+    len = pageArr.length.to_i - 1
+    pageArr.each_index do |i|
+      if currentPage == pageArr[i]
+        if i == 0
+          @nextPage = pageArr[i+1]
+        elsif i == len
+          @prevPage = pageArr[i-1]
+        else
+          @prevPage = pageArr[i-1]
+          @nextPage = pageArr[i+1]
+        end
+
+        break
+      end
+    end
+    # 分页 功能 end
+
   end
 end
