399164de38b0d3a497d3f4aae5cefe696c38a255
qiuguixin <qguixin@qq.com>
2017-12-19 01:29:04 +0800

    修复sidebar展开折叠不灵的bug

--- app/assets/javascripts/admin/common.js
+++ app/assets/javascripts/admin/common.js
@@ -7,3 +7,14 @@
 //= require dist/js/adminlte.min
 //= require_tree .
 //= require base
+
+function toggleSidebar() {
+    var $html = $('html');
+    var is_sidebar_collapsed = $html.hasClass('sidebar-collapse');
+    if (is_sidebar_collapsed) {
+        $html.removeClass('sidebar-collapse');
+    } else {
+        console.log('is_sidebar_collapsed:', is_sidebar_collapsed)
+        $html.addClass('sidebar-collapse');
+    }
+}
