2c9cb640aa0dded5d2c78e6edb37867e615759f3
qiuguixin <qguixin@qq.com>
2018-02-25 18:02:56 +0800

    小节详情页搞定

---
+++ app/assets/javascripts/posts.js
@@ -0,0 +1,109 @@
+$(function () {
+    var $article = $('.posts-show-container .markdown');
+    var $header = $article.find('h1, h2, h3');
+
+    // 目录生成
+    articleIndex();
+    function articleIndex() {
+        var _html = '<div class="nav-header"><span class="title">目录</span></div><div class="body"><div class="nav-body"><div id="postsActiveMenu" class="highlight-title" style="top:0;height:27px;display:block"></div><ul id="articleIndex" class="articleIndex"></ul></div></div>';
+        $('#postsCatalog').append(_html);
+        
+        var _tagLevel = 1;                  // 最初的level
+        var _$wrap = $('#articleIndex');    // 最初的wrap
+        $header.each(function (index) {
+            if ($(this).text().trim() === '') {     // 空的title
+                return;
+            }
+            $(this).attr('id', 'articleHeader' + index);      // 加id
+            var _tl = parseInt($(this)[0].tagName.slice(1));  // 当前的tagLevel
+            var _$li = null;
+            if (index === 0) { //第一个
+                _$li = $('<li class="active"><a href="#articleHeader' + index + '">' + $(this).text() + '</a></li>');
+                _$wrap.append(_$li);
+            } else if (_tl === _tagLevel) {  // 与上一个相同
+                _$li = $('<li><a href="#articleHeader' + index + '">' + $(this).text() + '</a></li>');
+                _$wrap.append(_$li);
+            } else if (_tl > _tagLevel) {  // 当前的大于上次的
+                _$li = $('<ul><li><a href="#articleHeader' + index + '">' + $(this).text() + '</a></li></ul>');
+                _$wrap.append(_$li);
+                _$wrap = _$li;
+            } else if (_tl < _tagLevel) {    // 当前的小于上次的
+                _$li = $('<li><a href="#articleHeader' + index + '">' + $(this).text() + '</a></li>');
+                if (_tl === 1) {
+                    $('#articleIndex').append(_$li);
+                    _$wrap = $('#articleIndex');
+                } else {
+                    _$wrap.parent('ul').append(_$li);
+                    _$wrap = _$wrap.parent('ul');
+                }
+            }
+            _tagLevel = _tl;
+        });
+    }
+    // 目录生成 end
+
+    //动态设置side的位置
+    setSidePosition();
+    $(window).scroll(function () {
+        setSidePosition()
+    })
+    function setSidePosition(){
+        var sideOffsetTop = $('.posts-show-container .side').offset().top - $(document).scrollTop();
+        var $side = $('.side-box-wrap');
+        // console.log('sideOffsetTop:', sideOffsetTop)
+        if (sideOffsetTop <= 0) {
+            $side.css({
+                position: 'fixed',
+                top: 0
+            })
+        } else {
+            $side.css({
+                position: 'static'
+            })
+        }
+    }
+    //动态设置side的位置end
+
+    //右侧导航链接高亮
+    $(window).scroll(function () {
+        var $menu = $("#articleIndex");
+        var top = $(document).scrollTop();
+        var currentId = ""; //滚动条现在所在位置的item id
+        $header.each(function () {
+            var m = $(this);
+            //注意：m.offset().top代表每一个item的顶部位置
+            if (top > m.offset().top - 30) {
+                currentId = "#" + m.attr("id");
+            } else {
+                return false;
+            }
+        });
+
+        var currentLi = $menu.find("li.active");
+        var currentLink = currentLi.find('a');
+        if (currentId && currentLink.attr("href") != currentId) {
+            currentLi.removeClass("active");
+            var $needActiveLi = $menu.find('[href="' + currentId + '"]').parent('li');
+            var titleOffsetTop = $needActiveLi.offset().top - $('#articleIndex').offset().top
+            $('#postsActiveMenu').css({
+                top: titleOffsetTop
+            })
+            $needActiveLi.addClass("active");
+        }
+        var footerOffsetTop = $('.footer').offset().top
+        console.log('footerOffsetTop', footerOffsetTop)
+        console.log('top', top)
+        if (top >= footerOffsetTop - window.innerHeight - 152 - 20){
+            $('.catalog-box').css({
+                height:'calc(100vh - 152px - 15px)'
+            })
+        }else {
+            $('.catalog-box').css({
+                height: '100vh'
+            })
+        }
+    });
+
+    //右侧导航链接高亮end
+});
+
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,5 @@
 module PostsHelper
+    def format_time(time_str)
+        time_str.strftime("%Y年%m月%d日 %H:%M:%S")
+    end
 end
