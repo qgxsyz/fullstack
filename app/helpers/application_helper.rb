a29b456292d54689ecca86e4066824515612a005
shistriver <954917356@qq.com>
2018-02-25 18:23:12 +0800

    制作登录注册弹窗

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,21 @@
 module ApplicationHelper
+  # 登录注册
+  def resource_name
+    :user
+  end
+
+  def resource
+      @resource ||= User.new
+  end
+
+  def resource_class
+      User
+  end
+
+  def devise_mapping
+      @devise_mapping ||= Devise.mappings[:user]
+  end
+  # 登录注册 end
 
   # 设置动态变化的网页标题
   def title(page_title)
